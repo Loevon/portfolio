93434c06864386740be0817b91bd7a0092498cc0
Matthew Hogetvedt <matthew.hogetvedt@compositeapps.net>
2018-04-24 12:56:58 -0700

    Adds bootstrap and sass to the project

---
+++ webapp/src/app/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login-view',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+	message = 'Login component works!';
+}
