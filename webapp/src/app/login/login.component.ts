18c80938d69f25da59f2ba6c85db51d936c4c791
Matthew Hogetvedt <matthew.hogetvedt@compositeapps.net>
2018-04-26 14:43:41 -0700

    Adds the ability to grab password for logging in

--- webapp/src/app/login/login.component.ts
+++ webapp/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    public errorStr: string = '';
+    private password: string = '';
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    public onLogin() {
+        if (this.password === 'test') {
+            // proceed to login
+        }
+        else {
+            this.onFailedPasswordAttempt();
+        }
+
+        // reset password window
+        this.password = '';
+    }
+
+    private onFailedPasswordAttempt() {
+        this.errorStr = 'Error String';
+    }
+
+    @HostListener('document:keypress', ['$event'])
+    handleKeyboardEvent(event: KeyboardEvent) {
+        if (event.code === "Enter" && this.password.length > 0) {
+            this.onLogin();
+        }
+    }
 }
