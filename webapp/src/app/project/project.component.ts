733aa2280b68e53fe5e4fa342065584cf175b21c
Matthew Hogetvedt <matthew.hogetvedt@compositeapps.net>
2018-05-04 09:14:03 -0700

    Project population now dynaimcally loads from list of projects via projects component

--- webapp/src/app/project/project.component.ts
+++ webapp/src/app/project/project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-project',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent implements OnInit {
-
+  @Input() project: any;
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  private openLink(link: string) {
+    // todo: transition to another webpage
+  }
 }
